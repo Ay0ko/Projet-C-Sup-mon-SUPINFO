944cac415653472c9918d3338396cff632fbe06a
Ay0ko <128031560+Ay0ko@users.noreply.github.com>
2025-02-05 13:36:38 +0100

    création struct spemon + début aventure

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+name[50];
+printf("Hello trainer, what's your name? \n");
+scanf("%s", name);
+printf("Hello %s !\n Welcome in Supemon World ! \n", name);
+
+struct Starter {
+    int lvl;
+    int HP;
+    int ATK;
+    int DEF;
+    int evasion;
+    int accuracy;
+    int speed;
+};
+
+int main(){
+    struct Starter Supmander = {1,10, 1, 1, 1, 2, 1};
+    struct Starter Supasaur = {1,9, 1, 1, 3, 2, 2};
+    struct Starter Supirtle = {1,11, 1, 2, 2, 1, 2};
+    struct Starter chosenSupemon;
+
+    printf("Choose your starter Supémon: \n");
+    printf("1 - Supmander (Balanced)\n");
+    printf("2 - Supasaur (Evasive)\n");
+    printf("3 - Supirtle (Defensive)\n");
+    printf("Enter 1, 2, or 3: ");
+
+     int choice;
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            chosenSupemon = Supmander;
+            printf("You chose Supmander!\n");
+            break;
+        case 2:
+            chosenSupemon = Supasaur;
+            printf("You chose Supasaur!\n");
+            break;
+        case 3:
+            chosenSupemon = Supirtle;
+            printf("You chose Supirtle!\n");
+            break;
+        default:
+            printf("Invalid choice! Chose your Supemon ! \n");
+    }
+
+
+    return 0;
+}
+
+
+
+
+
