440c638b90dab7ddb09adb2bd17408475a26e8b0
Aiqing <thomas.martin@supinfo.com>
2025-02-05 13:59:35 +0100

    Fix

--- main.c
+++ main.c
@@ -13,26 +13,13 @@ struct Starter {
     int speed;
 };
 
-int choix(poke1, poke2, poke3, pokechoisi){
-    int choice;
+struct Starter Supmander = {1,10, 1, 1, 1, 2, 1};
+struct Starter Supasaur = {1,9, 1, 1, 3, 2, 2};
+struct Starter Supirtle = {1,11, 1, 2, 2, 1, 2};
+struct Starter chosenSupemon;
+
+void choix(int choice){
     scanf("%d", &choice);
-    switch (choice) {
-        case 1:
-            pokechoisi = poke1;
-            printf("You chose Supmander!\n");
-            break;
-        case 2:
-            pokechoisi = poke2;
-            printf("You chose Supasaur!\n");
-            break;
-        case 3:
-            pokechoisi = poke3;
-            printf("You chose Supirtle!\n");
-            break;
-        default:
-            printf("Invalid choice! Chose your Supemon ! \n");
-            choix(poke1, poke2, poke3, pokechoisi);
-    }
 }
 
 int main(){
@@ -41,18 +28,30 @@ int main(){
     scanf("%s", name);
     printf("Hello %s !\n Welcome in Supemon World ! \n", name);
 
-    struct Starter Supmander = {1,10, 1, 1, 1, 2, 1};
-    struct Starter Supasaur = {1,9, 1, 1, 3, 2, 2};
-    struct Starter Supirtle = {1,11, 1, 2, 2, 1, 2};
-    struct Starter chosenSupemon;
-
     printf("Choose your starter Supémon: \n");
     printf("1 - Supmander (Balanced)\n");
     printf("2 - Supasaur (Evasive)\n");
     printf("3 - Supirtle (Defensive)\n");
     printf("Enter 1, 2, or 3: ");
 
-    choix(Supmander, Supasaur, Supirtle, chosenSupemon);
+    int choisi;
+    switch (choisi) {
+        case 1:
+            chosenSupemon = Supmander;
+            printf("You chose Supmander!\n");
+            break;
+        case 2:
+            chosenSupemon = Supasaur;
+            printf("You chose Supasaur!\n");
+            break;
+        case 3:
+            chosenSupemon = Supirtle;
+            printf("You chose Supirtle!\n");
+            break;
+        default:
+            printf("Invalid choice! Chose your Supemon ! \n");
+            choix(choisi);
+    }
 
 
 
