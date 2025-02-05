b680886698673e874743d839335e3ae04a0fb903
Aiqing <thomas.martin@supinfo.com>
2025-02-05 13:48:17 +0100

    Fix

--- main.c
+++ main.c
@@ -3,11 +3,6 @@
 #include <math.h>
 #include <string.h>
 
-name[50];
-printf("Hello trainer, what's your name? \n");
-scanf("%s", name);
-printf("Hello %s !\n Welcome in Supemon World ! \n", name);
-
 struct Starter {
     int lvl;
     int HP;
@@ -18,7 +13,34 @@ struct Starter {
     int speed;
 };
 
+int choix(poke1, poke2, poke3, pokechoisi){
+    int choice;
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            pokechoisi = poke1;
+            printf("You chose Supmander!\n");
+            break;
+        case 2:
+            pokechoisi = poke2;
+            printf("You chose Supasaur!\n");
+            break;
+        case 3:
+            pokechoisi = poke3;
+            printf("You chose Supirtle!\n");
+            break;
+        default:
+            printf("Invalid choice! Chose your Supemon ! \n");
+            choix(poke1, poke2, poke3, pokechoisi);
+    }
+}
+
 int main(){
+    char name[50];
+    printf("Hello trainer, what's your name ? \n");
+    scanf("%s", name);
+    printf("Hello %s !\n Welcome in Supemon World ! \n", name);
+
     struct Starter Supmander = {1,10, 1, 1, 1, 2, 1};
     struct Starter Supasaur = {1,9, 1, 1, 3, 2, 2};
     struct Starter Supirtle = {1,11, 1, 2, 2, 1, 2};
@@ -30,25 +52,8 @@ int main(){
     printf("3 - Supirtle (Defensive)\n");
     printf("Enter 1, 2, or 3: ");
 
-     int choice;
-    scanf("%d", &choice);
+    choix(Supmander, Supasaur, Supirtle, chosenSupemon);
 
-    switch (choice) {
-        case 1:
-            chosenSupemon = Supmander;
-            printf("You chose Supmander!\n");
-            break;
-        case 2:
-            chosenSupemon = Supasaur;
-            printf("You chose Supasaur!\n");
-            break;
-        case 3:
-            chosenSupemon = Supirtle;
-            printf("You chose Supirtle!\n");
-            break;
-        default:
-            printf("Invalid choice! Chose your Supemon ! \n");
-    }
 
 
     return 0;
